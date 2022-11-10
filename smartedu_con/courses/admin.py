32957c324358c5e2f88e6558acac498505529bf3
Alice <alicemkoyun@gmail.com>
2022-11-10 12:52:09 +0300

    Categories added into relational database and admin-side edited.

--- smartedu_con/courses/admin.py
+++ smartedu_con/courses/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from . models import Course
+from . models import Course,Category
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('name','available')
+    list_display = ('name','available','category')
     list_filter = ('available',)
-    search_fields = ('name','description')
+    search_fields = ('name','description','category')
+    
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
 
 # Register your models here.
